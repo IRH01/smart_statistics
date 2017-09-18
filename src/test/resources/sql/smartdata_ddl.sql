7eacd82a94f02740a9d456a6ae0f035eabb0076f
iritchie.ren <Renh1298@13322.com>
2017-09-18 17:47:14 +0800

    sql

--- src/test/resources/sql/smartdata_ddl.sql
+++ src/test/resources/sql/smartdata_ddl.sql
@@ -1,3 +1,12 @@
+DROP TABLE IF EXISTS tbl_admin;
+DROP TABLE IF EXISTS tbl_function;
+DROP TABLE IF EXISTS tbl_menu;
+DROP TABLE IF EXISTS tbl_permission;
+DROP TABLE IF EXISTS tbl_role;
+DROP TABLE IF EXISTS tbl_role_permission;
+DROP TABLE IF EXISTS tbl_user;
+DROP TABLE IF EXISTS tbl_user_role;
+
 DROP TABLE IF EXISTS interval_game_launch_report;
 DROP TABLE IF EXISTS interval_interface_report;
 DROP TABLE IF EXISTS interval_source_report;
@@ -11,10 +20,89 @@ DROP TABLE IF EXISTS month_register_report;
 DROP TABLE IF EXISTS month_recharge_report;
 DROP TABLE IF EXISTS month_loign_game_report;
 
+CREATE TABLE `tbl_admin` (
+   `ID` DOUBLE NOT NULL AUTO_INCREMENT COMMENT '主键',
+   `USER_ID` DOUBLE DEFAULT NULL COMMENT '用户ID',
+   `NAME` VARCHAR(80) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '名称',
+   `EMAIL` VARCHAR(200) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '邮箱',
+   `TEL` VARCHAR(60) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '电话',
+   `DEPART_ID` DOUBLE DEFAULT NULL COMMENT '部门ID(废弃)',
+   `JOB_NO` VARCHAR(60) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '工号',
+   `TYPE` DOUBLE DEFAULT NULL COMMENT '类型',
+   PRIMARY KEY (`ID`),
+   UNIQUE KEY `tbl_admin_pk` (`ID`)
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `tbl_function` (
+   `ID` DOUBLE NOT NULL AUTO_INCREMENT COMMENT '主键',
+   `PARENT_ID` DOUBLE DEFAULT NULL COMMENT '父功能点ID',
+   `NAME` VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '功能名称',
+   `FUNCTION_INDEX` DOUBLE DEFAULT NULL COMMENT '排序索引',
+   PRIMARY KEY (`ID`),
+   UNIQUE KEY `tbl_function_pk` (`ID`)
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='功能点表';
+
+CREATE TABLE `tbl_menu` (
+   `ID` DOUBLE NOT NULL AUTO_INCREMENT COMMENT '主键',
+   `PERMISSION` VARCHAR(400) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '权限ID',
+   `PARENT_ID` DOUBLE DEFAULT NULL COMMENT '父菜单ID',
+   `NAME` VARCHAR(400) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '菜单名称',
+   `URL` VARCHAR(800) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '菜单URL',
+   `MENU_INDEX` DOUBLE DEFAULT NULL COMMENT '排序索引',
+   `CREATE_TIME` DATETIME DEFAULT NULL COMMENT '创建时间',
+   PRIMARY KEY (`ID`),
+   UNIQUE KEY `tbl_menu_pk` (`ID`)
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='菜单表';
+
+CREATE TABLE `tbl_permission` (
+   `PERMISSION` VARCHAR(400) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '权限',
+   `NAME` VARCHAR(400) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '权限名称',
+   `FUNCTION_ID` DOUBLE DEFAULT NULL COMMENT '功能ID',
+   `CREATE_TIME` DATETIME DEFAULT NULL COMMENT '创建时间'
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='权限表';
+
+CREATE TABLE `tbl_role` (
+   `ID` DOUBLE NOT NULL AUTO_INCREMENT COMMENT '主键',
+   `NAME` VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '角色名称',
+   `CREATE_TIME` DATETIME DEFAULT NULL COMMENT '创建时间',
+   PRIMARY KEY (`ID`),
+   UNIQUE KEY `tbl_role_pk` (`ID`)
+) ENGINE=INNODB AUTO_INCREMENT=206 DEFAULT CHARSET=utf8 COMMENT='角色表';
+
+CREATE TABLE `tbl_role_permission` (
+   `ID` DOUBLE NOT NULL AUTO_INCREMENT COMMENT '主键',
+   `ROLE_ID` DOUBLE DEFAULT NULL COMMENT '角色ID',
+   `PERMISSION` VARCHAR(400) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '权限ID',
+   PRIMARY KEY (`ID`),
+   UNIQUE KEY `tbl_role_permission_pk` (`ID`)
+) ENGINE=INNODB AUTO_INCREMENT=1953 DEFAULT CHARSET=utf8 COMMENT='角色权限关系表';
+
+CREATE TABLE `tbl_user` (
+   `USER_ID` DOUBLE NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+   `USERNAME` VARCHAR(200) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '用户名称',
+   `PASSWORD` VARCHAR(200) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '密码',
+   `USER_STATUS` VARCHAR(8) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '用户状态',
+   `USER_TYPE` VARCHAR(8) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '用户类型',
+   `CREATE_TIME` DATETIME DEFAULT NULL COMMENT '创建时间',
+   `LOCALE` VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '语言（废弃）',
+   PRIMARY KEY (`USER_ID`),
+   UNIQUE KEY `tbl_user_pk` (`USER_ID`),
+   UNIQUE KEY `tbl_user_uk1` (`USERNAME`)
+) ENGINE=INNODB AUTO_INCREMENT=302 DEFAULT CHARSET=utf8 COMMENT='用户基本信息表';
+
+CREATE TABLE `tbl_user_role` (
+   `ID` DOUBLE NOT NULL AUTO_INCREMENT COMMENT '主键',
+   `USER_ID` DOUBLE DEFAULT NULL COMMENT '用户ID',
+   `ROLE_ID` DOUBLE DEFAULT NULL COMMENT '角色ID',
+   PRIMARY KEY (`ID`),
+   UNIQUE KEY `tbl_user_role_pk` (`ID`)
+) ENGINE=INNODB AUTO_INCREMENT=422 DEFAULT CHARSET=utf8 COMMENT='用户角色关系表';
+
 /*==============================================================*/
 /* Table: game_launch_statistic                                 */
 /*==============================================================*/
-CREATE TABLE interval_game_launch_report 
+CREATE TABLE interval_game_launch_report
 (
    id                   BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '平台实时统计，游戏启动统计',
    statistics_time      VARCHAR(10) NOT NULL DEFAULT '' COMMENT '统计时间(半小时)yyyy-MM-dd HH:30:00',
@@ -59,7 +147,7 @@ CREATE TABLE interval_source_report
 /*==============================================================*/
 /* Table: daily_composite_report                                */
 /*==============================================================*/
-CREATE TABLE daily_composite_report 
+CREATE TABLE daily_composite_report
 (
    id                   BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '日报表，综合报表',
    statistics_day       VARCHAR(10) NOT NULL DEFAULT '' COMMENT '统计日期(日)yyyy-MM-dd',
@@ -85,7 +173,7 @@ CREATE TABLE daily_composite_report
 /*==============================================================*/
 /* Table: daily_register_source_report                          */
 /*==============================================================*/
-create table daily_register_report 
+create table daily_register_report
 (
    id                   BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '日报表，注册来源统计报表',
    statistics_day       VARCHAR(10) NOT NULL DEFAULT '' COMMENT '统计日期(日)yyyy-MM-dd',
@@ -105,7 +193,7 @@ create table daily_register_report
 /*==============================================================*/
 /* Table: daily_loign_source_report                          */
 /*==============================================================*/
-create table daily_recharge_report 
+create table daily_recharge_report
 (
    id                   BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '日报表，充值来源统计报表',
    statistics_day       VARCHAR(10) NOT NULL DEFAULT '' COMMENT '统计日期(日)yyyy-MM-dd',
@@ -122,7 +210,7 @@ create table daily_recharge_report
 /*==============================================================*/
 /* Table: 登录来源统计                         */
 /*==============================================================*/
-create table daily_loign_game_report 
+create table daily_loign_game_report
 (
    id                   BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '日报表，充值来源统计报表',
    game_code            VARCHAR(10) NOT NULL DEFAULT '' COMMENT '游戏编码',
@@ -138,7 +226,7 @@ create table daily_loign_game_report
 /*==============================================================*/
 /* Table: 留存分析                         */
 /*==============================================================*/
-create table daily_Keep_record_report 
+create table daily_Keep_record_report
 (
    id                   BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '日报表，充值来源统计报表',
    statistics_day       VARCHAR(10) NOT NULL DEFAULT '' COMMENT '统计日期(日)yyyy-MM-dd',
@@ -160,7 +248,7 @@ create table daily_Keep_record_report
 /*==============================================================*/
 /* Table: month_composite_report                                */
 /*==============================================================*/
-CREATE TABLE month_composite_report 
+CREATE TABLE month_composite_report
 (
    id                   BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '月报表，综合报表',
    statistics_day       VARCHAR(10) NOT NULL DEFAULT '' COMMENT '统计日期(月)yyyy-MM',
@@ -186,7 +274,7 @@ CREATE TABLE month_composite_report
 /*==============================================================*/
 /* Table: month_register_source_report                          */
 /*==============================================================*/
-create table month_register_report 
+create table month_register_report
 (
    id                   BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '月报表，注册来源统计报表',
    statistics_day       VARCHAR(10) NOT NULL DEFAULT '' COMMENT '统计日期(月)yyyy-MM',
@@ -206,7 +294,7 @@ create table month_register_report
 /*==============================================================*/
 /* Table: month_loign_source_report                          */
 /*==============================================================*/
-create table month_recharge_report 
+create table month_recharge_report
 (
    id                   BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '月报表，充值来源统计报表',
    statistics_day       VARCHAR(10) NOT NULL DEFAULT '' COMMENT '统计日期(月)yyyy-MM',
@@ -223,7 +311,7 @@ create table month_recharge_report
 /*==============================================================*/
 /* Table: 登录来源统计                         */
 /*==============================================================*/
-create table month_loign_game_report 
+create table month_loign_game_report
 (
    id                   BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '日报表，充值来源统计报表',
    game_code            VARCHAR(10) NOT NULL DEFAULT '' COMMENT '游戏编码',
